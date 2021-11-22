add6758cb233c793c66721c9bb65e372c2669491
Guilherme Fonseca Leite <guilherme.leite1987@gmail.com>
2021-11-22 11:07:23 -0300

    Projeto Final Jogo de Xadrez

--- jogodexadrez/src/boardgame/Board.java
+++ jogodexadrez/src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
-			throw new BoardException("Erro creando tabuleiro: é necessário que haja pelo menos 1 linha e 1 coluna.");
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
@@ -25,14 +25,14 @@ public class Board {
 
 	public Piece piece(int row, int column) {
 		if (!positionExists(row, column)) {
-			throw new BoardException("Position not on the board.");
+			throw new BoardException("Position not on the board");
 		}
 		return pieces[row][column];
 	}
 
 	public Piece piece(Position position) {
 		if (!positionExists(position)) {
-			throw new BoardException("Position not on the board.");
+			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
@@ -45,6 +45,19 @@ public class Board {
 		piece.position = position;
 	}
 
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -55,7 +68,7 @@ public class Board {
 
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
-			throw new BoardException("Position not on the board.");
+			throw new BoardException("Position not on the board");
 		}
 		return piece(position) != null;
 	}
